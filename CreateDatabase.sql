a5c3c8e8287bbededc6c48bba2d2aba0c1630c83
jasonsippy <jasd4f@mail.missouri.edu>
2018-04-29 16:33:07 -0500

    Table Bugfix R3

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -5,6 +5,6 @@ CREATE TABLE Persons(
     Name varchar (50) NOT NULL,
     Age int NOT NULL,
     BodyWeight int NOT NULL,
-    DateRecorded date() NOT NULL,
+    DateRecorded date NOT NULL,
     BodyGroup varchar(50) NOT NULL
 );
