b4f076c324315b5fc2e3e25f9f00d74db321164c
jasonsippy <jasd4f@mail.missouri.edu>
2018-04-30 20:11:49 -0500

    Updated Persons, Added Exercise & Logbook

--- CreateDatabase.sql
+++ CreateDatabase.sql
@@ -1,9 +1,20 @@
-#FIT JOURNAL
-
 CREATE TABLE Persons(
-    Name varchar (50) NOT NULL,
+    ID int NOT NULL,
+    Name varchar(50) NOT NULL,
     Age int NOT NULL,
     BodyWeight int NOT NULL,
-    DateRecorded date NOT NULL,
-    BodyGroup varchar(50) NOT NULL
+    EntryDate date NOT NULL
+);
+
+CREATE TABLE Exercises(
+    Code char(3) NOT NULL,
+    ExerciseType varchar(50) NOT NULL
+);
+
+CREATE TABLE Logbook(
+    ID int NOT NULL,
+    Code char(3) NOT NULL,
+    Duration_(min) int NOT NULL,
+    Effort_(%) int NOT NULL,
+    Location varchar(100) NOT NULL DEFAULT ' '
 );
